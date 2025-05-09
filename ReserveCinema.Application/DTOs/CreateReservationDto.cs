95c53b83cb4bbe645e3823f3598b16cdb7f2cb49
DayanaraBV <dayanara.bone@uees.edu.ec>
2025-05-09 01:54:04 -0500

    feat(application): implementación caso de uso para crear reservas con validación de butacas disponibles

---
+++ ReserveCinema.Application/DTOs/CreateReservationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReserveCinema.Application.DTOs;
+
+public class CreateReservationDto
+{
+    public int ShowId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public List<int> SeatIds { get; set; } = new();
+}
---
+++ ReserveCinema.Application/UseCases/ReservationService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReserveCinema.Application.DTOs;
+using ReserveCinema.Application.Interfaces;
+using ReserveCinema.Domain.Entities;
+using ReserveCinema.Domain.Interfaces;
+
+namespace ReserveCinema.Application.UseCases;
+
+internal class ReservationService : IReservationService
+{
+    private readonly IReservationRepository _reservationRepo;
+    private readonly ISeatRepository _seatRepo;
+    private readonly IShowRepository _showRepo;
+
+    public ReservationService(
+        IReservationRepository reservationRepo,
+        ISeatRepository seatRepo,
+        IShowRepository showRepo)
+    {
+        _reservationRepo = reservationRepo;
+        _seatRepo = seatRepo;
+        _showRepo = showRepo;
+    }
+
+    public async Task<int> CreateReservationAsync(CreateReservationDto dto)
+    {
+        var show = await _showRepo.GetByIdAsync(dto.ShowId)
+            ?? throw new Exception("La función no existe.");
+
+        var availableSeats = await _seatRepo.GetAvailableSeatsAsync(dto.ShowId);
+        var selectedSeats = availableSeats.Where(s => dto.SeatIds.Contains(s.Id)).ToList();
+
+        if (selectedSeats.Count != dto.SeatIds.Count)
+            throw new Exception("Una o más butacas ya están ocupadas.");
+
+        var reservation = new Reservation
+        {
+            CustomerName = dto.CustomerName,
+            ReservedAt = DateTime.UtcNow,
+            ShowId = dto.ShowId,
+            ReservationSeats = selectedSeats.Select(seat => new ReservationSeat
+            {
+                SeatId = seat.Id
+            }).ToList()
+        };
+
+        await _reservationRepo.AddAsync(reservation);
+        await _seatRepo.ReserveSeatsAsync(dto.ShowId, dto.SeatIds);
+
+        return reservation.Id;
+    }
+}
