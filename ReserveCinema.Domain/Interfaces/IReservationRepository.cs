e02a11f7d869c660825d26f4c1087cb7e73730a9
DayanaraBV <dayanara.bone@uees.edu.ec>
2025-05-10 22:23:16 -0500

    feat(domain): se agrego interfaces de repositorio para Show, Seat y Reservation en la capa Domain

---
+++ ReserveCinema.Domain/Interfaces/IReservationRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReserveCinema.Domain.Entities;
+
+namespace ReserveCinema.Domain.Interfaces;
+
+public interface IReservationRepository
+{
+    Task<Reservation?> GetByIdAsync(int id);
+    Task<IEnumerable<Reservation>> GetAllAsync();
+    Task AddAsync(Reservation reservation);
+}
---
+++ ReserveCinema.Domain/Interfaces/ISeatRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReserveCinema.Domain.Entities;
+
+namespace ReserveCinema.Domain.Interfaces;
+
+public interface ISeatRepository
+{
+    Task<IEnumerable<Seat>> GetAvailableSeatsAsync(int showId);
+    Task ReserveSeatsAsync(int showId, IEnumerable<int> seatIds);
+}
